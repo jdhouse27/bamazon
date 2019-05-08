0e4a3e77ff56fdd1b80139986618facfbd73284c
JD <jdhouse27@gmail.com>
2019-05-08 13:57:16 -0500

    mods/got second prompt to work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
-
 const mysql = require("mysql");
-
 const keys = require("./keys.js")
 const bamazon = keys.bamazon;
 const {printTable} = require('console-table-printer');
+const inquirer = require("inquirer");
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -16,12 +15,11 @@ const connection = mysql.createConnection({
     // Password
     password: bamazon.password,
     database: "bamazonDB"
-  });
+});
 
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    // connection.end();
     Products();
   });
   
@@ -29,12 +27,59 @@ const connection = mysql.createConnection({
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err; 
       printTable(res);
-      
-      // for (let i = 0; i < res.length; i++){
-// 
-        // Log all results of the SELECT statement
-        // console.log(res[i] + '\n' + "====================================" + '\n');
-      // }
-      connection.end();
+      askID();
     });
   }
+
+function askID() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "itemID",
+      message: "What is the ID of the item you would like to purchase?"
+    }
+      ]).then(function(answer) {
+        let query = "SELECT * FROM products WHERE ?";
+        connection.query(query, { item_id: answer.itemID}, function(err, res) {
+          if (err) throw err; 
+
+          let quant = res[0].stock_quantity;
+          let prodID = res[0].item_id;
+          let prod = res[0].product_name;
+          let price = res[0].price;
+
+          if (quant > 0){
+          //log customer item
+          console.log("Item: " + prod + " | Price: " + price + " | Items available: " + quant);
+                    
+          //ask customer how many they would like to purchase
+            inquirer.prompt([
+              {
+                type: "input",
+                name: "quantity",
+                message: "How many would you like to purchase?"
+              },
+            ]).then(function(answer2) {
+
+              if (answer2.quantity <= quant){
+                let newQuant = quant - answer2.quantity;
+                let query2 = "UPDATE products SET ? WHERE item_id =" + prodID;
+                  connection.query(query2, {stock_quantity: newQuant}, function(err, res) {
+                if (err) throw err; 
+                console.log(res)
+                console.log("You have purchased " + answer2.quantity + " unit(s) of " + prod);
+                connection.end();
+                
+                  });
+                }
+              });
+
+          // quantity is 0, ask customer to select a new item
+        } else {
+          console.log("Sorry we currently do not have " + res[0].product_name + "in stock.  May we find you another item?")
+          askID();
+          }
+      });
+    });
+  }
+
